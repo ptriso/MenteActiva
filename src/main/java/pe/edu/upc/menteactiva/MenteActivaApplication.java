6d27650ad2359ef074f04405b2c5f82fb6d684ed
ptriso <u20201f237@upc.edu.pe>
2025-09-30 20:01:19 -0500

    Initial commit - Proyecto Spring Boot

---
+++ src/main/java/pe/edu/upc/menteactiva/MenteActivaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.menteactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenteActivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenteActivaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/menteactiva/MenteActivaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.menteactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenteActivaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
