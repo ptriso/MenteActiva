99c6aef19dfeaadfcdb1d9f1fb8716e598d95c9a
ptriso <u20201f237@upc.edu.pe>
2025-09-30 21:55:45 -0500

    fks, join y mapped by de users, authorities y tabla intermedia. Esas 3 entidades done
    3 entidades users, users_authorities, authorities. Done

--- src/main/java/pe/edu/upc/menteactiva/entities/User_Authority.java
+++ src/main/java/pe/edu/upc/menteactiva/entities/User_Authority.java
@@ -1,9 +1,11 @@
 package pe.edu.upc.menteactiva.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name="users_authorities")
@@ -15,6 +17,18 @@ public class User_Authority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonIgnore
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @JsonIgnore
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name="authority_id")
+    private Authority authority;
 
     
 }
